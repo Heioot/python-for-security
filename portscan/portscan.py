e88820479577f3ddb73ebd15dc3ca453ea8181da
Heioot <heitorluissmelo@gmail.com>
2023-11-05 13:50:27 -0300

    portscan

--- portscan/portscan.py
+++ portscan/portscan.py
@@ -1,16 +1,15 @@
 #portscan script feito com auxilio da aula
 
 import socket
-portas = [21, 23, 80, 443, 8080, 53, 25, 110, 135, 137, 138, 139, 1433, 1434, 3306, 3389, 5060, 5900, 8081, 8443, 10000]
+portas = [21, 23, 80, 443, 8080, 53, 25, 110, 135, 137, 138, 139, 1433, 1434, 3306, 3389, 5060, 5900, 8081, 8443, 10000] 
 
 
 local = input("Digite o host ou IP a ser verificado: ")
 
 for porta in portas: 
-    cliente = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    cliente = socket.socket(socket.AF_INET, socket.SOCK_STREAM) #AF_INET = IPV4, SOCK_STREAM = TCP
     cliente.settimeout(0.1)  #tempo de espera para a conexao, caso nao consiga, ele pula para a proxima porta
     resposta = cliente.connect_ex((local, porta)) # 0 Porta aberta, codigo = Porta fechada
 
     if resposta == 0:
         print(porta, "aberta")
-    
